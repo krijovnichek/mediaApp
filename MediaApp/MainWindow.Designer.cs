8ccfa24dfd2fa2adc807a9d043644e63b7bfd20d
Ruslan Tyukaev <tyukaevruslan@gmail.com>
2019-10-10 18:40:48 +0400

    Print Files directory

---
+++ MediaApp/MainWindow.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MediaApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.infoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Path = "E:\\Учеба 2019\\МПИСиТ\\FilesDir";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(228, 368);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infoLabel.Location = new System.Drawing.Point(428, 163);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(87, 26);
+            this.infoLabel.TabIndex = 1;
+            this.infoLabel.Text = "Nothing";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.listView1);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ MediaApp/MainWindow.cs
@@ -0,0 +1,54 @@
+﻿//using MetadataExtractor;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaApp
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // IEnumerable<Directory> directories = ImageMetadataReader.ReadMetadata(@"E:\Учеба 2019\МПИСиТ\FilesDir\13545844805_1fe22f9631_k.jpg");
+        String[] dir = Directory.GetFiles(@"E:\Учеба 2019\МПИСиТ\FilesDir\");
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Start new one");
+            foreach (var d in dir)
+            {
+                Console.WriteLine(d);
+            }
+                    
+
+
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, System.IO.FileSystemEventArgs e)
+        {
+           infoLabel.Text = ("Deleted");
+        }
+
+        private void fileSystemWatcher1_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            infoLabel.Text = ("Created");
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            infoLabel.Text = ("Changed");
+        }
+    }
+}
